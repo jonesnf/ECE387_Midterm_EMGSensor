0693329037d4245e6ca1fdde15f0bc87057d764e
jonesnf <jonesnf@miamioh.edu>
2017-03-14 00:45:46 -0400

    Working sorta

--- ECE387_MIDTERM_SM.ino
+++ ECE387_MIDTERM_SM.ino
@@ -2,19 +2,22 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define ST_GO        0x1                       //State 1 - increase in muscle activity, assuming walking if coming from idle state
-#define ST_STRIDE    0x2                       //State 2 - Mid stride - serves as reference point
-#define ST_PASS      0x3                       //State 3 - Pass position state - if two legs were implemented, 
+#define ST_INIT      0xF                       //Initializing emg sensor. First go 
+#define ST_RST       0xE                       //Reset
+#define ST_CAL       0x1                       //State Cal - calibration sate  
+#define ST_IDLE      0x2                       //Adding idle state as a no-op 
+#define ST_GO        0x3                       //State 1 - increase in muscle activity, assuming walking if coming from idle state
+#define ST_STRIDE    0x4                       //State 2 - Mid stride - serves as reference point
+#define ST_PASS      0x5                       //State 3 - Pass position state - if two legs were implemented, 
                                                   //this would be where the free foot passes the foot bearing the load
-#define ST_CAL       0x4                       //State Cal - calibration sate  
-#define ST_IDLE      0x5                       //Adding idle state as a no-op 
+                                              
 
-#define aPIN         0x0
+
+#define aPIN         0x0                       //analog pin to be used (A0)
 
 
 //Pins for I/O
 Servo myServo;
-//int aPIN = 0;                                  //analog pin to be used (A0)
 int pos = 0;
 int drive = 0;
 
@@ -29,6 +32,12 @@ boolean rxReady;
 //State Machine 
 int STATE = 0;
 
+//Values for implementing millis() timer
+int calibTime = 3000;
+long currentTime = 0;
+long resetTime = 0;
+long resetInterval = 0;
+
 
 void setup() {
   Serial.begin(9600);
@@ -42,7 +51,7 @@ void setup() {
         
   ADCSRA |= (1 << ADSC) | (1 << ADIE);      //Setting the Interrupt En. and Start conversion bits in our ADCSRA register
 
-  STATE = ST_CAL;                           //Setting the state to calibrate, when the emg sensor boots up it outputs high for a couple seconds
+  STATE = ST_INIT;                           //Setting the state to calibrate, when the emg sensor boots up it outputs high for a couple seconds
   
 }
 
@@ -66,63 +75,81 @@ void loop() {
   //Checking if we have recieved our data from A0, and we are ready to use it
   if(ADC_DONE){
 
-    switch(STATE){
+     //Reset checking
+     if(muscData == 0 && (STATE != ST_INIT || STATE != ST_RST)){
+        resetTime = millis();                 // time we reset               
+        resetInterval = resetTime + 3000;
+        STATE = ST_RST;
+        
+     }
+     
+     
+     switch(STATE){
+
+      case ST_RST:
+            resetTime = millis();
+            (resetTime >= resetInterval) ? STATE = ST_IDLE : ST_RST;
+            break;
+
+      case ST_INIT:
+            currentTime = millis();
+            (currentTime >= calibTime) ? STATE = ST_IDLE : ST_INIT;
+            break;
       
       case ST_CAL:
+      
            //do nothing.  need to hold position here while EMG sensor calibrates
-           STATE = ST_IDLE;
+           resetTime = millis();
+           Serial.print(F("Value: "));
+           Serial.println((muscData));
+           (resetTime >= resetInterval) ? STATE = ST_IDLE : ST_CAL;
            break;
+           
       case ST_IDLE:
+      
            myServo.writeMicroseconds(1500);
-           (muscData > oldVal && (muscData - oldVal >= 100) && STATE == ST_IDLE) ? STATE = ST_GO : STATE = ST_IDLE;           
+           Serial.print(F("Value: "));
+           Serial.println((muscData));
+           (muscData > oldVal && (muscData - oldVal)) ? STATE = ST_GO : STATE = ST_IDLE;           
            break;
+           
       case ST_GO:
+
+           Serial.print(F("Value: "));
+           Serial.println((muscData));
            drive = map(muscData, 0, 700, 120, 180);       //Scaling down our value from our EMG sensor from [0-700] to [0-180] ( > 90 to lift leg )
+           myServo.write(drive);
+           delay(200);
+           myServo.writeMicroseconds(1500);
+           STATE = ST_STRIDE;           
            break;
            
-           
+      case ST_STRIDE: 
       
-      
-      
-      
-      
-    
-    }
-      
+           //mid stride now, no need to do anything except check data
+           Serial.print(F("Value: "));
+           Serial.println((muscData));
+           //(muscData < oldVal && ( oldVal - muscData >= 75) && STATE == ST_STRIDE) ? STATE = ST_PASS : STATE = ST_STRIDE; 
+           (muscData < oldVal && ( oldVal - muscData >= 75)) ? STATE = ST_PASS : STATE = ST_STRIDE; 
+           break;
 
-/*
-      rxReady = false;
+      case ST_PASS: 
 
-      Serial.print("Old data:   ");
-      Serial.println(oldVal);
-      Serial.print("New data:   ");
-      Serial.println(muscData);
-
-      
-
-      if(muscData > oldVal && (muscData - oldVal) >= 100){
-           
-        
-           angle = map(muscData, 0, 700, 120, 179);
-           Serial.print("MOVE! ");
-           Serial.println(angle);
-           
-           myServo.write(angle); 
-           delay(250);
+           Serial.print(F("Value: "));
+           Serial.println((muscData));
+           drive = map(muscData, 0, 700, 0, 60);       //Scaling down our value from our EMG sensor from [0-700] to [0-180] ( > 90 to lift leg )
+           myServo.write(drive);
+           delay(200);
            myServo.writeMicroseconds(1500);
-           delay(10);
-        
-      }else{
-        angle = map(muscData, 0, 1023, 0, 90);
-        Serial.println("don't move!");
-        //myServo.write(angle);
-        delay(100);
-      }
-      
-      
-      ADC_DONE = false;
-      delay(10);
-      */
+           STATE = ST_IDLE;           
+           break;
+
+      default:
+           break;
+     
+     }
+      delay(700);
+      Serial.println(STATE);
       adcStarted = false;
     
     }
