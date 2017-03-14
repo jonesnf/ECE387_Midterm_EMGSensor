6ec4a3c2ebb93af2062828d9202a15661167b790
jonesnf <jonesnf@miamioh.edu>
2017-03-13 22:22:50 -0400

    simple update

--- ECE387_midterm.ino
+++ ECE387_midterm.ino
@@ -6,7 +6,7 @@
 Servo myServo;
 int aPIN = 0;                           //analog pin to be used (A0)
 int pos = 0;
-
+int angle = 0;
 
 //Interrupt values 
 volatile int muscData;
@@ -14,14 +14,19 @@ volatile boolean ADC_DONE;
 volatile int oldVal;
 volatile int newVal = 0;
 boolean adcStarted;
+boolean rxReady;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Initializing...");
   myServo.attach(9);
   
-  ADMUX = (1 << REFS0) | (aPIN & 0x07);
-  ADCSRA |= (1 << ADSC) | (1 << ADIE);
+  ADMUX = (0 << REFS1)                    
+        | (1 << REFS0)                      //Here we are selecting what reference we will be using (See datasheet for refences)
+        | (aPIN & 0x07);                    //Selecting A0 
+
+        
+  ADCSRA |= (1 << ADSC) | (1 << ADIE);      //Setting the Interrupt En. and Start conversion bits in our ADCSRA register
   
 }
 
@@ -41,42 +46,51 @@ ISR(ADC_vect){
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  //Checking if we have recieved our data from A0, and we are ready to use it
+  if(ADC_DONE && rxReady){
 
-  if(ADC_DONE){
+      rxReady = false;
 
-      adcStarted = false;
-
-      
       Serial.print("Old data:   ");
       Serial.println(oldVal);
       Serial.print("New data:   ");
       Serial.println(muscData);
+
       
 
-      if(muscData > oldVal){
-           pos+= 1;
-           Serial.println("move!");
-          myServo.write(90) ; 
+      if(muscData > oldVal && (muscData - oldVal) >= 100){
+           
+        
+           angle = map(muscData, 0, 700, 120, 179);
+           Serial.print("MOVE! ");
+           Serial.println(angle);
+           
+           myServo.write(angle); 
            delay(100);
+           myServo.writeMicroseconds(1500);
+           delay(10);
         
       }else{
-        pos-=1;
+        angle = map(muscData, 0, 1023, 0, 90);
         Serial.println("don't move!");
-        myServo.write(180);
+        //myServo.write(angle);
         delay(100);
       }
       
       
       ADC_DONE = false;
-    delay(1000);
+      delay(10);
+      
+      adcStarted = false;
     
     }
 
    if(!adcStarted){
 
-      ADCSRA |= bit(ADSC) | bit(ADIE);
+      ADCSRA |= (1 << ADSC) | (1 << ADIE);
       adcStarted = true;   
+      rxReady = true;
     
     }    
 
